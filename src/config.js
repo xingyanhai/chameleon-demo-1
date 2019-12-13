20488790ed67181a4d60d676384bd631006ad64a
xingyanhai <142xyh753869>
2019-12-13 14:25:37 +0800

    test-2

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dn: {
+    // 图片后缀
+    imageSuffix: '',
+    // webView的网址
+    webViewPath: 'https://mdev.dongni100.com',
+    title: '懂你教育',
+    NODE_ENV: 'production',
+    API_BASE_URL: 'https://mdev.dongni100.com',
+    CDN_URL: 'https://cdndev.dongni100.com/'
+  }
+}['dn']
